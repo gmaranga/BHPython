147d27e701bd74f2deaced46ed67cb5a7ed2c9b4
gustavo.maranga <gustavo.maranga@gmail.com>
2015-12-09 17:51:10 -0300

    Proxy finished. sshCmd using paramiko

--- securitytraining/proxy/SimpleTCPProxy.py
+++ securitytraining/proxy/SimpleTCPProxy.py
@@ -2,6 +2,7 @@ import sys
 import socket
 import threading
 
+
 def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -11,11 +12,11 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
         print "[!!] Check for other listening sockets or correct permissions."
         sys.exit(0)
 
+    server.listen(5)
     print "[*] Listening on %s:%d" % (local_host, local_port)
 
-    server.listen(5)
-
     while True:
+        print "[*]Accepting connections."
         client_socket, addr = server.accept()
         print "[==>] Received incoming connection from %s:%d" % (addr[0], addr[1])
 
@@ -23,7 +24,7 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
         proxy_thread = threading.Thread(target=proxy_handler, args=(client_socket, remote_host, remote_port, receive_first))
         proxy_thread.start()
 
-def main:
+def main():
     #no fancy command-line parsing here
     if len(sys.argv[1:]) != 5:
         print "Usage: ./SimpleTCPProxy.py [localhost] [localport] [remotehost] [remote port] [receive_first]"
@@ -48,6 +49,114 @@ def main:
 
     #now sping up our listening socket
     server_loop(local_host, local_port, remote_host, remote_port, receive_first)
+
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+    #connect to the remote host
+    print "[*] Connecting to remote host %s:%d" % (remote_host, remote_port)
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    remote_socket.connect((remote_host, remote_port))
+
+    #receive data from the remote end if necessary
+    #Some server daemons will expect you to do this first
+    # (FTP servers typically send a banner first, for example).
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+
+        #send it to our response handler
+        remote_buffer = response_handler(remote_buffer) #Inside this function, you can modify the packet contents,
+                        #perform fuzzing tasks, test for authentication issues, or whatever else your heart desires
+
+        #if we have data to send to our local client, send it
+        if len(remote_buffer):
+            print "[<==]Sending %d bytes to localhos" % len(remote_buffer)
+            client_socket.send(remote_buffer)
+    #now lets loop and read from local
+            #send to remote, send to local
+    #rinse, wash, repeat
+    while True:
+        #read from local host
+        local_buffer = receive_from(client_socket)
+
+        if len(local_buffer):
+            print "[==>] Received %d from localhost." % len(local_buffer)
+            hexdump(local_buffer)
+
+            #send it to our request handler
+            local_buffer = request_handler(local_buffer)#The same as response but for output traffic
+
+            #send off the data to the remote host
+            remote_socket.send(local_buffer)
+            print "[==>] Sent to remote."
+
+        #receive back the response
+        response_buffer = receive_from(remote_socket)
+
+        if len(remote_buffer):
+            print "[<==] Received %d bytes from remote." % len(remote_buffer)
+            hexdump(remote_buffer)
+
+            #send out to our response handler
+            remote_buffer = response_handler(remote_buffer)
+
+            #send the response to the local socket
+            client_socket.send(remote_buffer)
+
+            print "[<==] Sent to localhost."
+        #if no more data in either side, close the connections
+        if not len(local_buffer) or not len(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            print "[*] No more data. Closing connections."
+
+            break
+
+# this is a pretty hex dumping function directly taken from
+# the comments here:
+# http://code.activestate.com/recipes/142812-hex-dumper/
+def hexdump(src, length=16):
+    result = []
+    digits = 4 if isinstance(src, unicode) else 2
+    for i in xrange(0, len(src), length):
+        s = src[i:i+length]
+        hexa = b' '.join(["%0*X" % (digits, ord(x)) for x in s])
+        text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.' for x in s])
+        result.append(b"%04X %-*s %s" % (i, length*(digits+1), hexa, text))
+
+    print b'\n'.join(result)
+
+
+def receive_from(connection):
+
+    buffer = ""
+    #we set a 2 second timeout; depending on your target
+    #this may need to be adjusted
+    connection.settimeout(2)
+    try:
+        #keep reading into the buffer until
+        #there is no more data or we timeout
+        while True:
+            data = connection.recv(4096)
+
+            if not data:
+                break
+            buffer += data
+    except:
+        pass
+
+    return buffer
+
+#modify any requests destined for the remote host
+def request_handler(buffer):
+    #perform packet modifications
+    return buffer
+
+#modify any responses destined for the local host
+def response_handler(buffer):
+    #perform packet modifications
+    return buffer
+
 
 main()
 
